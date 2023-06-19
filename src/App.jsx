d7de845928432b7a5f88aa0c3996b65350702a0b
scottjudy <scott.judydev@outlook.com>
2023-06-19 10:25:47 +0200

    upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,27 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
+import Home from "./pages/home/Home";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [pokeData, setPokeData] = useState(null);
+
+  useEffect(() => {
+    fetch("https://pokeapi.co/api/v2/pokemon?limit=151")
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        setPokeData(data);
+      });
+  }, []);
+
   return (
     <>
-      <h1>hello von niklas</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,11 @@
+import "./Home.css";
+
+const Home = () => {
+  return (
+    <section>
+      <h2>home</h2>
+    </section>
+  );
+};
+
+export default Home;
