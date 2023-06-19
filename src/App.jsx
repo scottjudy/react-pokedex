2e0d032848ef0af38d24b8fc72311e6350c60c38
scottjudy <scott.judydev@outlook.com>
2023-06-19 12:01:56 +0200

    upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,8 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 import Home from "./pages/home/Home";
-import { useEffect, useState } from "react";
 
 function App() {
-  const [pokeData, setPokeData] = useState(null);
-
-  useEffect(() => {
-    fetch("https://pokeapi.co/api/v2/pokemon?limit=151")
-      .then((res) => res.json())
-      .then((data) => {
-        console.log(data);
-        setPokeData(data);
-      });
-  }, []);
-
   return (
     <>
       <BrowserRouter>
